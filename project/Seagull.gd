ced1015d8ce938f652d75003b19a4c4075198aaa
Chase W <spacechase0@users.noreply.github.com>
2020-04-02 21:39:42 -0400

    Seagull works now

--- project/Seagull.gd
+++ project/Seagull.gd
@@ -1,17 +1,27 @@
-extends RigidBody2D
+extends KinematicBody2D
 
-export var speed = 100
+export var speed : float = 100
+export var follow_path : NodePath
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+var velocity : Vector2 = Vector2.ZERO
 
+var path : Path2D
+var path_points
+var curr_path_point : int = 0
+var dir : int = 1
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
+func _ready() -> void:
+	path = get_node(follow_path) as Path2D
+	path_points = path.curve.get_baked_points()
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func _physics_process(delta) -> void:
+	var target = path.global_position + path_points[curr_path_point]
+	print(str(self.position)+" "+str(self.global_position))
+	while global_position.distance_to(target) < 16:
+		curr_path_point += dir
+		if curr_path_point < 0 || curr_path_point >= path_points.size():
+			dir = -dir
+			curr_path_point += dir
+		target = path_points[curr_path_point]
+	velocity = (target - position).normalized() * speed
+	velocity = move_and_slide(velocity)
