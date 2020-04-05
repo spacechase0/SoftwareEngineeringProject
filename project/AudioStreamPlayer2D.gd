06377ec0c4c33e37977761869c34266ed73febf0
Hollen Starr <cmg17d@my.fsu.edu>
2020-04-05 16:19:10 -0400

    added some sound effects and full theme

--- project/AudioStreamPlayer2D.gd
+++ project/AudioStreamPlayer2D.gd
@@ -1,16 +1,12 @@
 extends AudioStreamPlayer2D
 
+const Player = preload("res://Player.gd")
+var shoot_sound
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
-
-# Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	shoot_sound = get_node("bloop.wav")
+	set_process_input(true)
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func _input(event):
+	if Input.is_action_pressed("shoot"):
+		shoot_sound.play()
