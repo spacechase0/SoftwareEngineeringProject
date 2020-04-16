32d15c9e3d11175a765017f9d39ff7e10494a1df
Carissa Garde <47163038+beesbuzzingby@users.noreply.github.com>
2020-04-16 17:20:09 -0400

    Added new song
    Kind of darker than I wanted it to be, but it is a full melody like the first

--- project/AudioStreamPlayer.gd
+++ project/AudioStreamPlayer.gd
@@ -8,6 +8,8 @@ func _ready():
 	set_process(true)
 	adventureTheme2 = self.get_stream()
 	self.play()
+	if !self.playing:
+		self.play()
 
 #currently this just alternates between two of the melodies to demonstrate them
 #func _process(delta):
