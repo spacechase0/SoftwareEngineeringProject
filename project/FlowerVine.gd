393201ba28f1d045908171fd0299d26afa328564
Amanda Lovett <xeri0x0@gmail.com>
2020-04-17 13:59:15 -0400

    added visuals to flower boss and vine

--- project/FlowerVine.gd
+++ project/FlowerVine.gd
@@ -14,9 +14,14 @@ func _physics_process(delta : float):
 		vine_time -= delta
 		if vine_time <= 0:
 			($DisplayFull as Node2D).visible = true
+			$DisplayFull/AnimatedSprite.play("default");
 			($DisplaySprout as Node2D).visible = false
 			($CollisionShape2D as CollisionShape2D).disabled = false
+			
+
 	else:
 		vine_time -= delta
+		#$DisplayFull/AnimatedSprite.visible = false;
+		$DisplaySprout/vinetip.visible = true;
 		if vine_time <= -VINE_LIVE_TIME:
 			self.queue_free()
