380ac74726058d478150f2f5596335880b3ac40a
Amanda Lovett <xeri0x0@gmail.com>
2020-04-18 22:22:37 -0400

    new backgrounds and graphics, added cutscene

---
+++ project/Cutscene2.gd
@@ -0,0 +1,20 @@
+extends Node
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	$CanvasLayer/AnimatedSprite.play("default");
+	#test
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
+
+func _on_AnimatedSprite_animation_finished():
+	get_tree().change_scene("res://Level3.tscn")
